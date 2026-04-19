d513e852f3ff9d1376affca5c2d8748a3edaefb2
shashwat0224 <shashwat24baheti@gmail.com>
2026-04-19 21:50:08 +0530

    removed older version of the scraper

--- web_scraping/db.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = 'postgresql://postgres:20357@localhost:5432/train_db'
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
